782868acfc7fe14b378a14ba5ce98d24fde1c76a
gracephilipse <gracephilipse@outlook.com>
2023-07-25 16:23:19 +0800

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+require('dotenv').config();
+
+const userRoute = require('./routes/user.route'); 
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/user', userRoute); 
+
+module.exports = app;
