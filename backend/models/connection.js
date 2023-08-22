9ed65621448ef9f9f83ccc09bd8549704c402ab4
gracephilipse <gracephilipse@outlook.com>
2023-08-22 09:21:27 +0800

    production start

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,7 +1,7 @@
-const mysql = require("mysql")
+const mysql = require("mysql2")
 
 const client = mysql.createConnection({
-    host: "172.0.0.1",
+    host: "127.0.0.1",
     user: "root",
     password: "",
     port: 3306
