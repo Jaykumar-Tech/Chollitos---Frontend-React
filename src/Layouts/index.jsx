298411ab546a3446fdb427756a3e2ed7d843d320
gracephilipse <gracephilipse@outlook.com>
2023-08-24 03:40:40 +0800

    authentication

--- src/Layouts/index.jsx
+++ src/Layouts/index.jsx
@@ -49,6 +49,11 @@ export default function Navbar() {
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
   const [isSignInOpen, setIsSignInOpen] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rEmail, setrEmail] = useState("");
+  const [rPassword, setrPassword] = useState("");
+  const [rUsername, setrUsername] = useState("");
 
   const appMode = useBreakpointValue({ base: "sm", sm: "md", md: "lg" });
   const themeColor = "#007ea6"
@@ -65,63 +70,102 @@ export default function Navbar() {
   //   setIsSignUpOpen(true);
   // };
 
-  const handleSignInGoogle = () => {
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:4000/api/user/login",
+      {
+        email: email,
+        password: password
+      })
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:4000/api/user/register",
+      {
+        email: rEmail,
+        password: rPassword,
+        username: rUsername
+      })
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const handleSignInGoogle = (isLogin) => {
     const firebaseConfig = {
-			apiKey: "AIzaSyC2fD6TDBdbhZYg5097RAkMYs-7fJIPf_g",
-			authDomain: "chollo-es-396117.firebaseapp.com",
-			projectId: "chollo-es-396117",
-			storageBucket: "chollo-es-396117.appspot.com",
-			messagingSenderId: "416034001184",
-			appId: "1:416034001184:web:73027a3783bf8fd7e60745",
-			measurementId: "G-B9XRQE5YX9"
-		};
+      apiKey: "AIzaSyC2fD6TDBdbhZYg5097RAkMYs-7fJIPf_g",
+      authDomain: "chollo-es-396117.firebaseapp.com",
+      projectId: "chollo-es-396117",
+      storageBucket: "chollo-es-396117.appspot.com",
+      messagingSenderId: "416034001184",
+      appId: "1:416034001184:web:73027a3783bf8fd7e60745",
+      measurementId: "G-B9XRQE5YX9"
+    };
 
-		// Initialize Firebase
-		const app = initializeApp(firebaseConfig);
-		const analytics = getAnalytics(app);
-		const provider = new GoogleAuthProvider();
-		provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-		provider.setCustomParameters({
-			'login_hint': 'gemma@gmail.com'
-		  });
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const provider = new GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    provider.setCustomParameters({
+      'login_hint': 'gemma@gmail.com'
+    });
 
-		// Initialize Firebase Authentication and get a reference to the service
-		const auth = getAuth(app);
-		signInWithPopup(auth, provider)
-			.then((result) => {
-				// This gives you a Google Access Token. You can use it to access the Google API.
-				const credential = GoogleAuthProvider.credentialFromResult(result);
-				console.log(result);
-				// The signed-in user info.
-				const user = result.user;
-				result.user.getIdToken(true).then(function(idToken){
-					axios.post("http://5.75.224.135:4000/api/user/google",{
-						idToken: idToken,
-						email: user.email
-					})
-					.then(response=>{
-						console.log(response) ;
-					})
-					.catch(err=>{
-						console.log(err)
-					})
-				}).catch(function(err){
-					console.log(err);
-				})
-				
-				// IdP data available using getAdditionalUserInfo(result)
-				// ...
-			}).catch((error) => {
-				// Handle Errors here.
-				console.log(error)
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				// The email of the user's account used.
-				const email = error.customData.email;
-				// The AuthCredential type that was used.
-				const credential = GoogleAuthProvider.credentialFromError(error);
-				// ...
-			});
+    // Initialize Firebase Authentication and get a reference to the service
+    const auth = getAuth(app);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        console.log(result);
+        // The signed-in user info.
+        const user = result.user;
+        if (isLogin) {
+
+          result.user.getIdToken(true).then(function (idToken) {
+            axios.post("http://localhost:4000/api/user/google", {
+              idToken: idToken,
+              email: user.email
+            })
+              .then(response => {
+                console.log(response);
+                  console.log("login success")
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          }).catch(function (err) {
+            console.log(err);
+          })
+        } else {
+          setrEmail(user.email);
+          setrUsername(user.displayName);
+        }
+    
+
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        console.log(error)
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
   }
 
   const handleSignUpCloseModal = () => {
@@ -276,7 +320,7 @@ export default function Navbar() {
               </Button>
               <Button
                 // onClick={onClick}
-                onClick={handleSignInGoogle}
+                onClick={() => handleSignInGoogle(true)}
                 bg="red.500"
                 color="white"
                 _hover={{ bg: 'red.600' }}
@@ -286,33 +330,40 @@ export default function Navbar() {
                 <Text ml={3}>Continue with Google</Text>
               </Button>
               <Spacer height={4} />
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Password</FormLabel>
-                <Input type="password" />
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack
-                  direction={{ base: "column", sm: "row" }}
-                  align={"start"}
-                  justify={"space-between"}
-                >
-                  <Checkbox>Remember me</Checkbox>
-                  <Text color={"blue.400"}>Forgot password?</Text>
+              <form onSubmit={handleSignIn}>
+                <FormControl id="email">
+                  <FormLabel>Email address</FormLabel>
+                  <Input type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)} />
+                </FormControl>
+                <FormControl id="password">
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)} />
+                </FormControl>
+                <Stack spacing={10}>
+                  <Stack
+                    direction={{ base: "column", sm: "row" }}
+                    align={"start"}
+                    justify={"space-between"}
+                  >
+                    <Checkbox>Remember me</Checkbox>
+                    <Text color={"blue.400"}>Forgot password?</Text>
+                  </Stack>
+                  <Button
+                    bg={"blue.400"}
+                    color={"white"}
+                    _hover={{
+                      bg: "blue.500",
+                    }}
+                    type="submit"
+                  >
+                    Sign in
+                  </Button>
                 </Stack>
-                <Button
-                  bg={"blue.400"}
-                  color={"white"}
-                  _hover={{
-                    bg: "blue.500",
-                  }}
-                >
-                  Sign in
-                </Button>
-              </Stack>
+              </form>
               <Stack pt={6}>
                 <Text align={'center'}>
                   Don't have an account? <Text color={'blue.400'} onClick={toSignUp}>Signup</Text>
@@ -345,6 +396,7 @@ export default function Navbar() {
               </Button>
               <Button
                 // onClick={onClick}
+                onClick={() => handleSignInGoogle(false)}
                 bg="red.500"
                 color="white"
                 _hover={{ bg: 'red.600' }}
@@ -354,42 +406,51 @@ export default function Navbar() {
                 <Text ml={3}>Continue with Google</Text>
               </Button>
               <Spacer height={4} />
-              <Box>
-                <FormControl id="name" isRequired>
-                  <FormLabel>User Name</FormLabel>
-                  <Input type="text" />
+              <form onSubmit={handleSignUp}>
+                <Box>
+                  <FormControl id="name" isRequired>
+                    <FormLabel>User Name</FormLabel>
+                    <Input type="text"
+                      value={rUsername}
+                      onChange={e => setrUsername(e.target.value)} />
+                  </FormControl>
+                </Box>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
+                  <Input type="email"
+                    value={rEmail}
+                    onChange={e => setrEmail(e.target.value)} />
                 </FormControl>
-              </Box>
-              <FormControl id="email" isRequired>
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password" isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                  <Input type={showPassword ? 'text' : 'password'} />
-                  <InputRightElement h={'full'}>
-                    <Button
-                      variant={'ghost'}
-                      onClick={() => setShowPassword((showPassword) => !showPassword)}
-                    >
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <Stack spacing={10} pt={2}>
-                <Button
-                  loadingText="Submitting"
-                  size="lg"
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Sign up
-                </Button>
-              </Stack>
+                <FormControl id="password" isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <InputGroup>
+                    <Input type={showPassword ? 'text' : 'password'}
+                      value={rPassword}
+                      onChange={e => setrPassword(e.target.value)} />
+                    <InputRightElement h={'full'}>
+                      <Button
+                        variant={'ghost'}
+                        onClick={() => setShowPassword((showPassword) => !showPassword)}
+                      >
+                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                <Stack spacing={10} pt={2}>
+                  <Button
+                    type="submit"
+                    loadingText="Submitting"
+                    size="lg"
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'blue.500',
+                    }}>
+                    Sign up
+                  </Button>
+                </Stack>
+              </form>
               <Stack pt={6}>
                 <Text align={'center'}>
                   Already have an account? <Text color={'blue.400'} onClick={toSignIn}>Signin</Text>
